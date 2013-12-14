9a3f121bbb7f94bc9d0954b19f9d66d3e146ce6e
Johannes Link <business@johanneslink.net>
2013-12-14 13:32:36 +0100

    added contract to WordWrapper.wrap

--- src/wordwrap/WordWrapper.groovy
+++ src/wordwrap/WordWrapper.groovy
@@ -1,10 +1,12 @@
 package wordwrap
 
+import org.gcontracts.annotations.Ensures
 import org.gcontracts.annotations.Requires
 
 class WordWrapper {
 
     @Requires({ wrapAt > 0 })
+    @Ensures({ result instanceof List<String> })
     def wrap(text, int wrapAt) {
         []
     }
