badc5fb2badc836d9d911d2d9fc8c000c44fadd2
Johannes Link <business@johanneslink.net>
2014-01-16 16:48:50 +0100

    Added recognition of serveral digits in a row

--- src/bankOCR/us1/OCR.groovy
+++ src/bankOCR/us1/OCR.groovy
@@ -1,7 +1,50 @@
 package bankOCR.us1
 
+import groovy.transform.CompileStatic
+
 class OCR {
+
+    private final static Map<Integer, List<String>> patterns = [
+            1: ['   ', '  |', '  |'],
+            2: [' _ ', ' _|', '|_ ']
+    ]
+
     int recognizeDigit(List<String> digitGrid) {
-        1
+        patterns.find { key, value ->
+            patternMatchesDigit(value, digitGrid)
+        }?.key ?: -1
+    }
+
+    @CompileStatic
+    private boolean patternMatchesDigit(List<String> pattern, List<String> digit) {
+        for (int lineIndex = 0; lineIndex < 3; lineIndex++) {
+            if (pattern[lineIndex] != digit[lineIndex].padRight(3))
+                return false
+        }
+        return true
+    }
+
+    @CompileStatic
+    //lineGrid must contain 3 lines
+    List<Integer> recognizeLine(List<String> lineGrid) {
+        def digitGrids = cutIntoDigits(lineGrid)
+        digitGrids.collect { List<String> digitGrid ->
+            recognizeDigit(digitGrid)
+        }
+    }
+
+    @CompileStatic
+    private cutIntoDigits(List<String> lineGrid) {
+        def digits = []
+        int numberOfDigits = (lineGrid[0].size() / 3).intValue()
+        if (lineGrid[0].size() % 3 != 0)
+            numberOfDigits++
+        for (int i = 0; i < numberOfDigits * 3; i += 3) {
+            def digit = lineGrid.collect { String line ->
+                return line.padRight(numberOfDigits * 3).substring(i, i + 3)
+            }
+            digits << digit
+        }
+        return digits
     }
 }
--- tests/bankOCR/us1/OCRSpec.groovy
+++ tests/bankOCR/us1/OCRSpec.groovy
@@ -6,16 +6,38 @@ import spock.lang.Unroll
 
 class OCRSpec extends Specification {
 
+    OCR ocr
+
+    def setup() {
+        ocr = new OCR()
+    }
+
     @Unroll
-    def "recognize #grid as digit #digit"() {
+    def "recognize #grid as digit #digit #comment"() {
         expect:
-            def ocr = new OCR()
             ocr.recognizeDigit(grid) == digit
         where:
-            grid                  | digit
-            ['   ', '  |', '  |'] | 1
-            [' _ ', ' _|', '|_ '] | 2
+            grid                  | digit | comment
+            ['   ', '  |', '  |'] | 1     | ''
+            [' _ ', ' _|', '|_ '] | 2     | ''
+            [' _', ' _|', '|_']   | 2     | 'without trailing spaces'
+            ['***', '***', '***'] | -1    | '(unknown)'
 
     }
+
+    def "recognize line with two digits"() {
+        given:
+            def lineGrid = [
+                    '    _',
+                    '  | _|',
+                    '  ||_'
+            ]
+        when:
+            def digits = ocr.recognizeLine(lineGrid)
+        then:
+            digits == [1, 2]
+    }
+
+    //todo: Lines are much shorter (e.g. many 1s at end)
 
 }
